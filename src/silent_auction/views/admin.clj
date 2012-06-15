e4b10c318b46f209fe68f84507734f8315f232d0
Trent Ogren <tedwardo2@gmail.com>
2012-06-15 17:05:06 -0500

    Use hiccup-bootstrap table helper

--- src/silent_auction/views/admin.clj
+++ src/silent_auction/views/admin.clj
@@ -2,7 +2,34 @@
   (:use hiccup.core
         hiccup.page
         hiccup.bootstrap.page)
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as str]))
+
+;; from https://github.com/weavejester/hiccup-bootstrap/blob/master/src/hiccup/bootstrap/element.clj
+
+(defn- classes-from-keys [prefix keys]
+  (->> keys
+       (map #(str prefix (name %)))
+       (str/join " ")))
+
+(defn table
+  "Create a Bootstrap table with the supplied styles, head and body. The head
+  should be a sequence of column names. The body should be a seq of seqs,
+  containing the table data. The styles should be a seq of keywords of the set
+  #{:bordered :striped :condensed}.
+
+  See: http://twitter.github.com/bootstrap/base-css.html#tables"
+  [& {:keys [styles head body]}]
+  (let [classes (str "table " (classes-from-keys "table-" styles))]
+    [:table {:class classes}
+     [:thead
+      [:tr
+       (for [col head] [:th col])]]
+     [:tbody
+      (for [row body]
+        [:tr
+         (for [cell row] [:td cell])])]]))
+
+;;;;;;;
 
 (defn navbar []
   (html
@@ -30,48 +57,44 @@
 
 (defn- city-state-zip
   [city state zip]
-  (let [city-state (string/join ", " (remove nil? [city state]))]
-    (string/join " " (remove string/blank? [city-state zip]))))
+  (let [city-state (str/join ", " (remove nil? [city state]))]
+    (str/join " " (remove str/blank? [city-state zip]))))
 
 (defn- item-row [{:keys [complete
-                        category
-                        item
-                        description
-                        donor-name
-                        name-for-listing
-                        address
-                        city
-                        state
-                        zip
-                        solicited-by
-                        fair-market-value]}]
-  [:tr
-    [:td complete]
-    [:td category]
-    [:td item]
-    [:td description]
-    [:td donor-name]
-    [:td name-for-listing]
-    [:td address]
-    [:td (city-state-zip city state zip)]
-    [:td solicited-by]
-    [:td fair-market-value]])
+                         category
+                         item
+                         description
+                         donor-name
+                         name-for-listing
+                         address
+                         city
+                         state
+                         zip
+                         solicited-by
+                         fair-market-value]}]
+  [complete
+   category
+   item
+   description
+   donor-name
+   name-for-listing
+   address
+   (city-state-zip city state zip)
+   solicited-by
+   fair-market-value])
 
 (defn items [itms]
   (layout
     [:div.page-header [:h1 "Auction Items"]]
-    [:table.table.table-bordered.table-striped
-      [:thead
-        [:tr
-          [:th "Item Complete?"]
-          [:th "Category"]
-          [:th "Item"]
-          [:th "Description"]
-          [:th "Donor Name"]
-          [:th "Name for Listing"]
-          [:th "Address"]
-          [:th "City, State, Zip"]
-          [:th "Solicited by"]
-          [:th "Fair Mkt. Value"]]]
-      [:tbody
-        (map item-row itms)]]))
+    (table :styles #{:bordered :striped}
+           :head ["Item Complete?"
+                  "Category"
+                  "Item"
+                  "Description"
+                  "Donor Name"
+                  "Name for Listing"
+                  "Address"
+                  "City, State, Zip"
+                  "Solicited by"
+                  "Fair Mkt. Value"]
+           :body (map item-row itms))))
